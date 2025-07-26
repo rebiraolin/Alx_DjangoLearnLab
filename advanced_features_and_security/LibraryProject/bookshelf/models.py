94742dbc618e79c2ae698dcf127286b82fee39ee
Rebira Oli <rebiranegassa24@gmail.com>
2025-07-26 17:14:23 +0300

    update

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,17 +1,9 @@
-# advanced_features_and_security/bookshelf/models.py
-
 from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager # Consolidated imports
-from django.utils.translation import gettext_lazy as _ # Good practice for verbose names
-
-# --- START CustomUserManager and CustomUser definitions ---
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(UserManager):
-    # If you want to use email as the primary login, you can adjust create_user/superuser
-    # to make email required and set USERNAME_FIELD = 'email' in CustomUser.
-    # For now, keeping it as it was for username-based AbstractUser.
     def create_user(self, username, email=None, password=None, **extra_fields):
-        # Ensure email is normalized if it's used as a unique identifier
         if email:
             email = self.normalize_email(email)
         return super().create_user(username, email, password, **extra_fields)
@@ -38,10 +30,7 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-# --- END CustomUserManager and CustomUser definitions ---
 
-
-# Existing Book Model (ensure this remains as it was, now below CustomUser)
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -52,7 +41,6 @@ class Book(models.Model):
             ("can_add_book", "Can add book"),
             ("can_change_book", "Can change book"),
             ("can_delete_book", "Can delete book"),
-            # Add the new permissions for Task 1, Step 1 if you haven't yet
             ("can_view", "Can view book"),
             ("can_create", "Can create book"),
             ("can_edit", "Can edit book"),
@@ -61,9 +49,3 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author}"
-
-# If you have Author, Library, Librarian, UserProfile models in relationship_app/models.py,
-# they should NOT be duplicated here unless bookshelf specifically needs its own versions.
-# Based on your previous relationship_app/models.py, those models belong there.
-# Only include models here that are specifically part of the 'bookshelf' app's domain.
-# I'm assuming for now that the Author, Library, Librarian, UserProfile are only in relationship_app.
