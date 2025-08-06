17d59045d294f65da3b31add55bb1cfb254a1b9b
Rebira Oli <rebiranegassa24@gmail.com>
2025-08-06 12:09:47 +0300

    update

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,8 @@
+from django.shortcuts import render
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 
 
 
