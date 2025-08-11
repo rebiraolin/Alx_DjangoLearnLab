008f648faf0ae96bef60d0e985bf5c24068af203
Rebira Oli <rebiranegassa24@gmail.com>
2025-08-11 11:08:07 +0300

    update

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -72,11 +72,22 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+#
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
