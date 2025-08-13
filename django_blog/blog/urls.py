7d3cc59667607c3485018f962eb2811a252da25e
Rebira Oli <rebiranegassa24@gmail.com>
2025-08-13 11:22:46 +0300

    Update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name = 'post_new'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = 'post_delete'),
-    path('post/<int:post_pk>/comments/new/', CommentCreateView.as_view(), name = 'comment_create'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name = 'comment_create'),
     path('post/<int:post_pk>/comment/<int:pk>/update/', CommentUpdateView.as_view(), name = 'comment_update'),
     path('post/<int:post_pk>/comment/<int:pk>/delete/', CommentDeleteView.as_view(), name = 'comment_delete'),
 
